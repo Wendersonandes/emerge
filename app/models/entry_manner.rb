a3b05fdcd1e8da306e06801d2d530c221c95139d
Wendersonandes <oldmaverick@gmail.com>
2001-01-01 12:00:17 -0200

    Removed correios_ou_internet from enum options

--- app/models/entry_manner.rb
+++ app/models/entry_manner.rb
@@ -1,6 +1,6 @@
 class EntryManner < ActiveRecord::Base
   belongs_to :opportunity
 
-	enum entry_type: { correios: 0, online: 1, email: 2, indicacao: 3, nao_definido: 4, correios_ou_internet: 5 }
+	enum entry_type: { correios: 0, online: 1, email: 2, indicacao: 3, nao_definido: 4 }
 	validates :entry_type, :presence => true
 end
