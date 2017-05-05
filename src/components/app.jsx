9daaf3bba49626d92001ce777e545a3d3e312da9
Wendersonandes <oldmaverick@gmail.com>
2017-05-05 12:28:22 -0300

    Removed menu from app.jsx

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
-import Menu from '../shared/menu';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Menu />
         {this.props.children}
       </div>
     );
