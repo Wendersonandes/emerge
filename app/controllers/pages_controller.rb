6384dcb5de7d361433c26f6c5f923e56e078a3ad
Wendersonandes <oldmaverick@gmail.com>
2017-05-08 13:24:12 -0300

    Added wide layout to pages_controller.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_authorization_check
+  #skip_authorization_check
   skip_before_action :authenticate_user!
 	layout "wide", :only => [:about, :adds]
 
