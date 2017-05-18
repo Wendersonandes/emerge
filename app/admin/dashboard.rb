24baabd7f73fd2ff1cfb1185e740361da2dd50f0
Wendersonandes <oldmaverick@gmail.com>
2017-05-18 11:22:28 -0300

    Added new opportunity form to dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,12 +3,51 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+
+		columns do
+			column do
+       panel "Oportunidades mais visualizadas" do
+          table_for Opportunity.most_viewed.limit(5) do
+            column("Título") {|opportunity| link_to(opportunity.title, admin_opportunity_path(opportunity)) }
+						column("Visualizações")   {|opportunity| opportunity.impressionist_count(:filter=>:ip_address) }
+          end
+        end
+			end
+			column span: 2 do
+				panel "Adicionar uma nova oportunidade" do
+					render :partial => "opportunity_form"
+				end
+			end
+		end
+
+    columns do
+
+      column do
+        panel "Oportunidades mais visualizadas" do
+          table_for Opportunity.most_viewed.limit(5) do
+            column("Título"){|opportunity| link_to(opportunity.title, admin_opportunity_path(opportunity)) }
+						column("Visualizações")   {|opportunity| opportunity.impressionist_count(:filter=>:ip_address) }
+          end
+        end
       end
-    end
+
+      column do
+        panel "Usuário novos" do
+          table_for User.order('id desc').limit(10).each do |user|
+            column(:email)    {|user| link_to(user.email, admin_user_path(user)) }
+          end
+        end
+      end
+
+      column do
+        panel "Usuários que mais acessam" do
+          table_for User.order('sign_in_count desc').limit(5).each do |user|
+            column(:email)    {|user| link_to(user.email, admin_user_path(user)) }
+						column("Número de acessos") {|user| user.sign_in_count}
+          end
+        end
+      end
+		end
 
     # Here is an example of a simple dashboard with columns and panels.
     #
