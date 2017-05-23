68e43cd43fb36396db69a55527a64000ecd923d6
Wendersonandes <oldmaverick@gmail.com>
2017-05-23 08:44:21 -0300

    Working on download_list helper

--- app/decorators/opportunity_decorator.rb
+++ app/decorators/opportunity_decorator.rb
@@ -26,16 +26,38 @@ class OpportunityDecorator < ApplicationDecorator
 		markdown(object.content)
 	end
 
-	def downloads
-		object.docs.each do |doc|
-			concat h.content_tag(:li, h.link_to(doc.doc_url))
-		end	
+	def category_list_formated
+		if object.category_list.present?
+			h.content_tag(:div, :class => "col-sm-12") do
+			object.category_list.map{ |tag|
+				h.concat(h.content_tag(:span, tag, :class => "label label-info mr2"))
+			}
+			end
+		else
+			h.content_tag(:div,"Categoria não informada", :class => "col-sm-12 white")
+		end
 	end
 
+	def download_list
+		if object.docs.present?
+			h.content_tag(:div) do
+				h.concat h.content_tag(:button,"Downloads", :class => "btn btn-sm btn-primary dropdown-toggle", :data => {:toggle => "dropdown"}) do
+				h.content_tag(:span,"",:class => "caret")
+			end
+				h.concat h.content_tag(:ul,"", :class => "dropdown-menu") do
+					object.docs.each do |doc|	
+						h.content_tag(:li, doc.doc_type)
+					end
+				end
+			end
+		else
+			h.content_tag(:button,"Não existe nenhum documento",:type => "button", :class => "btn btn-sm btn-primary")
+		end
+	end
 
 	private
 
 	def handle_date(value, message="Data não informada")
-		value.present? ? h.local_time_ago(value) + h.content_tag(:span, "(#{value})", :class => "ml3") : message
+		value.present? ? h.local_time_ago(value) + h.content_tag(:span, "(#{value.strftime(" %d/%m/%y ")})", :class => "ml3") : message
 	end
 end
