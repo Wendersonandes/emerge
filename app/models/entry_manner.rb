4ce03154a5408beeb3d2921ecc87dd4df94c66bf
Wendersonandes <oldmaverick@gmail.com>
2017-05-25 08:05:57 -0300

    Removed accentuation from entry_types

--- app/models/entry_manner.rb
+++ app/models/entry_manner.rb
@@ -1,4 +1,4 @@
 class EntryManner < ActiveRecord::Base
   belongs_to :opportunity
-	enum entry_type: { correios: 0, online: 1, email: 2, indicação: 3, não_definido: 4, correios_ou_internet: 5 }
+	enum entry_type: { correios: 0, online: 1, email: 2, indicacao: 3, nao_definido: 4, correios_ou_internet: 5 }
 end
