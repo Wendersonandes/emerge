cbcc101caa4c4274c073122a5e3178ebb094c48b
Wendersonandes <oldmaverick@gmail.com>
2017-06-17 14:46:19 -0300

    Fixed doc menu download button and cleaned h6 tags on header

--- app/cells/opportunity_cell/header_cell.rb
+++ app/cells/opportunity_cell/header_cell.rb
@@ -36,7 +36,7 @@ module OpportunityCell
 			handle_date(result)
 		end
 
-		def grants_formatted
+		def total_awards_value
 			grants.present? ? humanized_money_with_symbol(value_of_awards) : "Não informado"
 		end
 
@@ -65,18 +65,60 @@ module OpportunityCell
 				end
 				result
 			else
-				"Forma de Inscrição não informada"
+				content_tag(:h6) do
+					"<span class=\"glyphicon glyphicon-envelope mr2\"></span> Forma de Inscrição: Não informada"
+				end
+			end
+		end
+
+		def render_docs_button_formatted
+			if docs.present? 
+				content_tag(:button, :type => "button", :class => "btn btn-sm btn-primary", :"dropdown-toggle" => nil, :data => {:toggle => "dropdown"}) do
+					result = ""
+					result << "<span class=\"glyphicon glyphicon-folder-open mr2\"></span>"
+					result << "Downloads"
+					result << "<span class=\"caret\"></span>"
+				end
+			else
+				content_tag(:button, :type => "button", :class => "btn btn-sm btn-default") do
+					result = ""
+					result << "<span class=\"glyphicon glyphicon-folder-open mr2\"></span>" 
+					result << "Não existe documento"
+				end
+			end
+		end
+
+		def render_docs_button_menu
+			if docs.present? 
+				content_tag(:ul, :class => "dropdown-menu") do
+					result = ""
+					docs.each do |doc|
+						result << content_tag(:li) do
+							link_to doc.doc_url do 
+								"<span class=\"glyphicon glyphicon-save\"></span> #{doc.doc_type}"
+							end
+						end
+					end
+					result
+				end
 			end
 		end
 
 		private
+
+		def doc_link_with_icon(doc)
+			link_to doc.doc_url do 
+				"<span class=\"glyphicon glyphicon-save\"></span>"
+				doc.doc_type
+			end
+		end
 
 		def handle_date(value, message="Data não informada")
 			if value.present? 
 				content_tag(:span, :class => "ml2") do
 					result = ""
 					result << value.to_s
-					result << content_tag(:span,nil, :class => "cronogram ml2", :data => {:datetime => "(#{value})"})
+					result << content_tag(:span,nil, :class => "cronogram ml2", :data => {:datetime => value})
 				end
 			else
 				content_tag(:span, message, :class => "ml2")
