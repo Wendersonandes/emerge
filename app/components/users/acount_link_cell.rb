1da23db7e7ab091227f27118dfccc8565a46cdde
Wendersonandes <oldmaverick@gmail.com>
2017-07-10 19:28:57 -0300

    Added label to user profile_skills

--- app/components/users/acount_link_cell.rb
+++ app/components/users/acount_link_cell.rb
@@ -4,9 +4,18 @@ module Users
 		property :name
 		property :email
 		property :person
+		property :profile_skill_list
 
 		def show
 			model ? render : ""
+		end
+
+		def profile_skill_list_formated
+			if person.profile_skill_list.present?
+				person.profile_skill_list.map{ |tag|
+					content_tag(:span, tag, :class => "label label-info mr2")  
+				}
+			end
 		end
 
 	end
