745540df06fabd86384b8f2dc6014c0df9371355
Wendersonandes <oldmaverick@gmail.com>
2017-07-13 15:57:48 -0300

    Added missing mailer url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,11 +66,11 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] }
+  config.action_mailer.default_url_options = { host: 'www.emerge.art.br' }
   config.action_mailer.smtp_settings = {
     user_name:      ENV['SENDGRID_USERNAME'],
     password:       ENV['SENDGRID_PASSWORD'],
-    domain:         ENV['MAIL_HOST'],
+		domain:         'www.emerge.art.br',
     address:       'smtp.sendgrid.net',
     port:          '587',
     authentication: :plain,
