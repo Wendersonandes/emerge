58585276bae25f89ee3e6e2b2b883ee056947530
Wendersonandes <oldmaverick@gmail.com>
2017-07-18 22:55:07 -0300

    Added sucker punch config option to devise async

--- config/initializers/devise_async.rb
+++ config/initializers/devise_async.rb
@@ -1,7 +1,7 @@
 if defined? Devise::Async
   Devise::Async.setup do |config|
     # config.enabled = false
-    config.backend = :sidekiq
+    config.backend = :sucker_punch
     config.queue = :mailer
   end
 end
