1dba6d2e091137fabad4dbc3dbe7406bc5bcbcbc
Wendersonandes <oldmaverick@gmail.com>
2017-07-28 16:39:22 -0300

    Created docs cell

---
+++ app/components/opportunities/header/docs_button_cell.rb
@@ -0,0 +1,34 @@
+module Opportunities
+	module Header
+		class DocsButtonCell < Cell::ViewModel
+			property :doc_type
+			property :doc_url
+
+			def show
+				render
+			end
+
+			def item
+				render
+			end
+
+			def render_docs_button_formatted
+				if model.present? 
+					result = ""
+					result << content_tag(:button, :type => "button", :class => "btn btn-sm btn-primary", :"dropdown-toggle" => nil, :data => {:toggle => "dropdown"}) do
+						"<span class=\"glyphicon glyphicon-folder-open mr2\"></span> Downloads<span class=\"caret\"></span>"
+					end
+					result << content_tag(:ul, :class => "dropdown-menu") do
+						cell(Opportunities::Header::DocsButtonCell, :collection => model).(:item)
+					end
+					result
+				else
+					content_tag(:button, :type => "button", :class => "btn btn-sm btn-default") do
+						"<span class=\"glyphicon glyphicon-folder-open mr2\"></span>  Não existe documento" 
+					end
+				end
+			end
+
+		end
+	end
+end
